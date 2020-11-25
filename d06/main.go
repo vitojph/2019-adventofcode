b25c3a57a136da65ffeb22c3d477c39fcc945f4f
Víctor Peinado <vitojph@gmail.com>
2020-11-25 20:09:14 +0100

    Add solution for day 6, part 2

--- d06/main.go
+++ d06/main.go
@@ -8,17 +8,56 @@ import (
 	"strings"
 )
 
-// orbitMap contains an orbit map
 type orbitMap map[string]map[string]int
+
+type path []string
+
+func (p path) position(planet string) int {
+	for i, v := range p {
+		if v == planet {
+			return i
+		}
+	}
+	return 10000
+}
+
+// shortestPath computes the shortest path between two points
+func (o orbitMap) shortestPath(p1, p2 string) int {
+	path1 := make(path, 0)
+	path2 := make(path, 0)
+	o.orbitsToCOM(p1, &path1)
+	o.orbitsToCOM(p2, &path2)
+
+	intersection1 := len(path1)
+	intersection2 := len(path2)
+	for i, planet := range path1 {
+		if path2.position(planet) < intersection2 {
+			intersection1 = i
+			intersection2 = path2.position(planet)
+		}
+	}
+	return len(path1[:intersection1]) + len(path2[:intersection2])
+}
 
 // orbitsToCOM computes the number of indirect orbits
 // from a given planet to COM
-func (o orbitMap) orbitsToCOM(planet string) int {
+func (o orbitMap) orbitsToCOM(planet string, p *path) {
+	if planet != "COM" {
+		for k := range o[planet] {
+			*p = append(*p, k)
+			o.orbitsToCOM(k, p)
+		}
+	}
+}
+
+// nOrbitsToCOM computes the number of indirect orbits
+// from a given planet to COM
+func (o orbitMap) nOrbitsToCOM(planet string) int {
 	total := 0
 	if planet != "COM" {
 		for k := range o[planet] {
 			total++
-			total += o.orbitsToCOM(k)
+			total += o.nOrbitsToCOM(k)
 		}
 	}
 	return total
@@ -75,11 +114,15 @@ func main() {
 	fmt.Println("Day 6")
 
 	orbits := loadOrbits("input.txt")
+	//fmt.Printf("%v\n", orbits)
 	direct := orbits.directOrbits()
 	indirect := 0
 	for k := range orbits {
-		indirect += orbits.orbitsToCOM(k)
+		indirect += orbits.nOrbitsToCOM(k)
 	}
 	fmt.Println("Direct orbits:", direct)
 	fmt.Println("Total orbits:", indirect)
+
+	transfers := orbits.shortestPath("SAN", "YOU")
+	fmt.Println("Number of orbital transfers:", transfers)
 }
